64af444ed4dee1604b4cd54cf68a97c988675b3b
Amir Khan <ak2022dev@gmail.com>
2022-11-02 12:09:05 +0000

    Add tests for post sort-names with body rather than query parameters. Passed

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -23,7 +23,7 @@ describe Application do
     end
   end
 
-  context "POST to /sort-names" do
+  context "POST to /sort-names with query params" do
     it "returns 200 with names sorted" do
       # Send a get request to /sort-names
       # and returns a response we can test
@@ -37,6 +37,27 @@ describe Application do
       # Send a get request to /sort-names
       # and returns a response we can test
       response = post("/sort-names?names=Zebedee,Alex,Tom,Karen,Jennie")
+  
+      # Assert the response status code and body
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Alex,Jennie,Karen,Tom,Zebedee")
+    end  
+  end
+
+  context "POST to /sort-names with body params" do
+    it "returns 200 with names sorted" do
+      # Send a get request to /sort-names
+      # and returns a response we can test
+      response = post("/sort-names", names: "Joe,Alice,Zoe,Julia,Kieran")
+
+      # Assert the response status code and body
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Alice,Joe,Julia,Kieran,Zoe")
+    end
+    it "returns 200 with names sorted" do
+      # Send a get request to /sort-names
+      # and returns a response we can test
+      response = post("/sort-names", names: "Zebedee,Alex,Tom,Karen,Jennie")
   
       # Assert the response status code and body
       expect(response.status).to eq(200)
